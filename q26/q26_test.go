4184bd9ac8a113cec3200200d2c2bac0725f7a24
Pruthvi Anvesh <pruthvianveshmuga@github.com>
2022-01-19 15:43:57 +0530

    refactor tests q26

--- q26/q26_test.go
+++ q26/q26_test.go
@@ -14,13 +14,13 @@ func TestSolutions(t *testing.T) {
 	solutions := []func([]int) int{Solution1, Solution2}
 
 	for solInd, sol := range solutions {
-		// Cannot move this to the top of the loop, because the test cases are not deep-copied.
-		testCases := []Case{
-			{[]int{1,1,2}, 2},
-			{[]int{0,0,1,1,1,2,2,3,3,4}, 5},
-		}
 		t.Run("s"+strconv.Itoa(solInd+1), func(t *testing.T) {
-			for tcInd, tc := range testCases {
+			// Use deepCopyTestCases() if you move the testcases to the top level.
+			tcs := 	[]Case{
+				{[]int{1,1,2}, 2},
+				{[]int{0,0,1,1,1,2,2,3,3,4}, 5},
+			}
+			for tcInd, tc := range tcs {
 				t.Run("c"+strconv.Itoa(tcInd+1), func(t *testing.T) {
 					if res := sol(tc.inp); res != tc.out {
 						t.Errorf("sol%v, testCase%v, %v: expected %v, got %v", solInd, tcInd, tc.inp, tc.out, res)
@@ -29,4 +29,15 @@ func TestSolutions(t *testing.T) {
 			}
 		})
 	}
+}
+
+func deepCopyTestCases(testCases []Case) []Case {
+	tcs := make([]Case, len(testCases))
+	for tcInd, tc := range testCases {
+		inp := make([]int, len(tc.inp))
+		copy(inp, tc.inp)
+		out := tc.out
+		tcs[tcInd] = Case{inp, out}
+	}
+	return tcs
 }
