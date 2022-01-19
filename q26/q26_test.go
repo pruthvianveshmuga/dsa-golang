e6a9d125ef257e69527b533cb0dd6d5ff16b5f93
Pruthvi Anvesh <pruthvianveshmuga@github.com>
2022-01-19 15:23:46 +0530

    refactor tests

--- q26/q26_test.go
+++ q26/q26_test.go
@@ -1,23 +1,32 @@
 package q26
 
 import (
+	"strconv"
 	"testing"
 )
 
-func TestSolution1(t *testing.T) {
-	inp := []int{1,1,2}
-	out := 2
-	res := Solution1(inp)
-	if res != out {
-		t.Errorf("got %v, want %v", res, out)
-	}
+type Case struct {
+	inp  []int
+	out int
 }
 
-func TestSolution2(t *testing.T) {
-	inp := []int{1,1,2}
-	out := 2
-	res := Solution2(inp)
-	if res != out {
-		t.Errorf("got %v, want %v", res, out)
+func TestSolutions(t *testing.T) {
+	solutions := []func([]int) int{Solution1, Solution2}
+
+	for solInd, sol := range solutions {
+		// Cannot move this to the top of the loop, because the test cases are not deep-copied.
+		testCases := []Case{
+			{[]int{1,1,2}, 2},
+			{[]int{0,0,1,1,1,2,2,3,3,4}, 5},
+		}
+		t.Run("s"+strconv.Itoa(solInd+1), func(t *testing.T) {
+			for tcInd, tc := range testCases {
+				t.Run("c"+strconv.Itoa(tcInd+1), func(t *testing.T) {
+					if res := sol(tc.inp); res != tc.out {
+						t.Errorf("sol%v, testCase%v, %v: expected %v, got %v", solInd, tcInd, tc.inp, tc.out, res)
+					}
+				})
+			}
+		})
 	}
 }
