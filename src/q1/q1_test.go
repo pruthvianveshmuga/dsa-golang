1ce73d673a2e6f6dd97ed1e4405496a06ddd78fd
Pruthvi Anvesh <pruthvianveshmuga@github.com>
2022-01-20 15:51:13 +0530

    add q1: two sum

---
+++ src/q1/q1_test.go
@@ -0,0 +1,35 @@
+package q1
+
+import (
+	"strconv"
+	"testing"
+
+	"github.com/pruthvianveshmuga/dsa/utils"
+)
+
+type Case struct {
+	inp1 []int
+	inp2 int
+	out []int
+}
+
+func TestSolutions(t *testing.T) {
+	solutions := []func([]int, int) []int{Solution1}
+
+	for solInd, sol := range solutions {
+		t.Run("s"+strconv.Itoa(solInd+1), func(t *testing.T) {
+			tcs := 	[]Case{
+				{[]int{2,7,11,15}, 9, []int{0,1}},
+				{[]int{3,2,4}, 6, []int{1,2}},
+				{[]int{3,3}, 6, []int{0,1}},
+			}
+			for tcInd, tc := range tcs {
+				t.Run("c"+strconv.Itoa(tcInd+1), func(t *testing.T) {
+					if res := sol(tc.inp1, tc.inp2); !utils.DeepEqual(res, tc.out) {
+						t.Errorf("sol%v, testCase%v, %v & %v: expected %v, got %v", solInd, tcInd, tc.inp1, tc.inp2, tc.out, res)
+					}
+				})
+			}
+		})
+	}
+}
