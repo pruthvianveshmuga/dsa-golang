7ccbe87dcaa3d19c42e85b98cf2d8bf880c994ac
Pruthvi Anvesh Muga <39563717+pruthvianveshmuga@users.noreply.github.com>
2022-01-28 11:56:10 +0530

    sol1 (#4)
    Co-authored-by: Pruthvi Anvesh <pruthvianveshmuga@github.com>

--- src/q48/solution1.go
+++ src/q48/solution1.go
@@ -1,5 +1,26 @@
 package q48
 
+import "github.com/pruthvianveshmuga/dsa/utils"
+
+func flipDiagonally(matrix [][]int) {
+	n := len(matrix)
+	for i := 0; i < n; i++ {
+		for j := 0; j < n-i-1; j++ {
+			utils.Swap(&matrix[i][j], &matrix[n-j-1][n-i-1])
+		}
+	}
+}
+
+func flipVertically(matrix [][]int) {
+	n := len(matrix)
+	for i := 0; i < n/2; i++ {
+		for j := 0; j < n; j++ {
+			utils.Swap(&matrix[i][j], &matrix[n-i-1][j])
+		}
+	}
+}
+
 func Solution1(matrix [][]int) {
-	
+	flipDiagonally(matrix)
+	flipVertically(matrix)
 }
