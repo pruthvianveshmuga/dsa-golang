a8969170a70f7a115eb01519578d4cb987a99708
Pruthvi Anvesh <pruthvianveshmuga@github.com>
2022-01-28 11:55:29 +0530

    sol1

--- src/q48/q48_test.go
+++ src/q48/q48_test.go
@@ -13,7 +13,7 @@ type Case struct {
 }
 
 func TestSolutions(t *testing.T) {
-	solutions := []func([][]int) {}
+	solutions := []func([][]int) {Solution1}
 
 	for solInd, sol := range solutions {
 		t.Run("s"+strconv.Itoa(solInd+1), func(t *testing.T) {
